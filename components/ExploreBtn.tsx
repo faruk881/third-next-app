6b3c81507d3803e65ad7bb71eea3d6738eb36dbc
Md. Omar Faruk <omarfaruk.likhon@gmail.com>
2026-04-09 10:34:21 +0600

    Update ExploreBtn component to comment out the arrow-down image for better layout control

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -7,7 +7,7 @@ const ExploreBtn = () => {
     <button type="button" id="explore-btn" className="mt-7" onClick={()=>console.log('clicked')}>
         <a href="#events">
             Explore events
-            <Image src="/icons/arrow-down.svg" alt="arrow-down" width={24} height={24}/>
+            {/* <Image src="/icons/arrow-down.svg" alt="arrow-down" width={14} height={24}/> */}
         </a>
     </button>
   )
